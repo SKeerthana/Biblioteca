fb8e333d2f6966c58bbd0606375aca0b57e44695
keerthas <keerthas@INkeerthas.local>
2015-09-04 15:44:37 +0530

    Spec for printing list of books message

--- test/com/thoughtworks/biblioteca/DisplayTest.java
+++ test/com/thoughtworks/biblioteca/DisplayTest.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.biblioteca;
 
 import org.junit.Test;
+
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
@@ -9,7 +10,7 @@ import static org.junit.Assert.assertEquals;
 public class DisplayTest {
 
     @Test
-    public void shouldPrintTheMessagePassed() {
+    public void shouldPrintTheWelcomeMessagePassed() {
         ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         PrintStream printStream = new PrintStream(outContent);
         Display display = new Display(printStream);
@@ -17,5 +18,17 @@ public class DisplayTest {
 
         display.printMessage("Welcome to Biblioteca");
         assertEquals("Welcome to Biblioteca\n", outContent.toString());
+    }
+
+    @Test
+    public void shouldPrintTheListOfBooks() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outContent);
+        Display display = new Display(printStream);
+        System.setOut(printStream);
+        display.printMessage("My experiments with Truth\t" + "M.K.Gandhi\t" + "1942\n"
+                + "Harry Potter and the Chamber of Secrets\t" + "J. K. Rowling\t" + "1998\n");
+        assertEquals("My experiments with Truth\t" + "M.K.Gandhi\t" + "1942\n"
+                + "Harry Potter and the Chamber of Secrets\t" + "J. K. Rowling\t" + "1998\n", outContent.toString());
     }
 }
