4e5e96bc8d375e8a3216cdf3a1bc05c1325faf17
keerthas <keerthas@INkeerthas.local>
2015-09-05 06:52:19 +0530

    Handling the selected menu option

--- src/com/thoughtworks/biblioteca/Menu.java
+++ src/com/thoughtworks/biblioteca/Menu.java
@@ -4,7 +4,16 @@ public class Menu {
 
     public String getMenuOptions() {
         String menuOptions = "Menu options :\n";
-        menuOptions += "List all the books\n";
+        menuOptions += "1. List all the books\n";
         return menuOptions;
+    }
+
+    public int handleSelectedMenuOption(int choice) {
+        switch (choice) {
+            case 1:
+                return 1;
+            default:
+                return 0;
+        }
     }
 }
--- test/com/thoughtworks/biblioteca/MenuTest.java
+++ test/com/thoughtworks/biblioteca/MenuTest.java
@@ -10,7 +10,13 @@ public class MenuTest {
     public void shouldReturnTheListOFMenuOptions() {
         Menu menu = new Menu();
         String menuOptions = "Menu options :\n";
-        menuOptions += "List all the books\n";
+        menuOptions += "1. List all the books\n";
         assertEquals(menuOptions, menu.getMenuOptions());
+    }
+
+    @Test
+    public void shouldHandleTheSelectedOption() {
+        Menu menu = new Menu();
+        assertEquals(1,menu.handleSelectedMenuOption(1));
     }
 }
