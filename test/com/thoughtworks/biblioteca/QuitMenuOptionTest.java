1a705575e003161dfd79bc876964882323adb2a8
keerthas <keerthas@INkeerthas.local>
2015-09-09 12:34:53 +0530

    spec for quit menu option using system rules lib

--- test/com/thoughtworks/biblioteca/QuitMenuOptionTest.java
+++ test/com/thoughtworks/biblioteca/QuitMenuOptionTest.java
@@ -1,11 +1,17 @@
 package com.thoughtworks.biblioteca;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
 
 public class QuitMenuOptionTest {
+    @Rule
+    public final ExpectedSystemExit exit = ExpectedSystemExit.none();
 
     @Test
-    public void shouldCallExit() {
-        //anshu
+    public void shouldExitIfOptionTwoIsChosen() {
+        exit.expectSystemExit();
+        QuitMenuOption quitMenuOption = new QuitMenuOption();
+        quitMenuOption.performOperation();
     }
 }
