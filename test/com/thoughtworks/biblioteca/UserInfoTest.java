64c8746e2c09a7d892abbf76093776e86ac3fa57
keerthas <keerthas@INkeerthas.local>
2015-09-15 14:36:26 +0530

    Spec to check if two different users with different username are different

--- test/com/thoughtworks/biblioteca/UserInfoTest.java
+++ test/com/thoughtworks/biblioteca/UserInfoTest.java
@@ -24,4 +24,11 @@ public class UserInfoTest {
         UserInfo userInfo2 = new UserInfo("ABC", "abd", "gsdjsjahjk");
         assertEquals(userInfo1, userInfo2);
     }
+
+    @Test
+    public void shouldReturnFlaseWhenUserNameIsDifferent() {
+        UserInfo userInfo1 = new UserInfo("ABC", "ABC", "abc@gmail.com");
+        UserInfo userInfo2 = new UserInfo("ABC23", "abd", "gsdjsjahjk");
+        assertNotEquals(userInfo1, userInfo2);
+    }
 }
