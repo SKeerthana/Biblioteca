9b74c0dcbcd7cc36cc6190ee3386f9d3645c1134
keerthas <keerthas@INkeerthas.local>
2015-09-16 06:45:07 +0530

    Spec to check login menu option for Invalid credentials

--- test/com/thoughtworks/biblioteca/LoginMenuOptionTest.java
+++ test/com/thoughtworks/biblioteca/LoginMenuOptionTest.java
@@ -32,4 +32,22 @@ public class LoginMenuOptionTest {
 
         assertEquals("Enter Library Number : Enter Password : Login Successful", outContent.toString());
     }
+
+    @Test
+    public void shouldNotLoginForInValidCredentials() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outContent);
+        System.setOut(printStream);
+        String input = "1234-122" + "\n" + "abc12";
+        ByteArrayInputStream inContent = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inContent);
+        ConsoleDisplay consoleDisplay = new ConsoleDisplay(printStream, System.in);
+        UserManager userManager = new UserManager(validUsers);
+
+        LoginMenuOption loginMenuOption = new LoginMenuOption(consoleDisplay, userManager);
+
+        loginMenuOption.performOperation();
+
+        assertEquals("Enter Library Number : Enter Password : ", outContent.toString());
+    }
 }
