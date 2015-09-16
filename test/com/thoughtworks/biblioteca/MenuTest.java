6c11c4b1a319c1621a20507317688a440d5ee0e4
keerthas <keerthas@INkeerthas.local>
2015-09-16 16:56:57 +0530

    Spec to check for menu options for normal user

--- test/com/thoughtworks/biblioteca/MenuTest.java
+++ test/com/thoughtworks/biblioteca/MenuTest.java
@@ -32,6 +32,21 @@ public class MenuTest {
     }
 
     @Test
+    public void shouldReturnTheListOfMenuOptionsForNormalUsers() {
+        Menu menu = new Menu(listOfMenuOptions);
+        String menuOptions = "Choose one of the menu option :\n";
+        menuOptions += "List Books\n";
+        menuOptions += "Quit\n";
+        menuOptions += "CheckOut Book\n";
+        menuOptions += "Return Book\n";
+        menuOptions += "List Movies\n";
+        menuOptions += "CheckOut Movie\n";
+        menuOptions += "User Information\n";
+        menuOptions += "Log out\n";
+        assertEquals(menuOptions, menu.getMenuOptions(new UserInfo("lib-0001", "name", "abc", "abc@gmail.com", Role.USER)));
+    }
+
+    @Test
     public void shouldHandleListOfBooksOption() {
         ListLibraryItemMenuOption listLibraryItemMenuOption = mock(ListLibraryItemMenuOption.class);
         Menu menu = new Menu(listOfMenuOptions);
