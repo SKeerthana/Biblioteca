add91217304c96b4a902736da6058c4b602338e2
keerthas <keerthas@INkeerthas.local>
2015-09-16 05:54:30 +0530

    Added spec to check for invalid library number login

--- test/com/thoughtworks/biblioteca/UserManagerTest.java
+++ test/com/thoughtworks/biblioteca/UserManagerTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.HashMap;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class UserManagerTest {
@@ -17,5 +18,16 @@ public class UserManagerTest {
         UserManager userManager = new UserManager(validUsers);
 
         assertTrue(userManager.authenticate("1234-122", "abc"));
+    }
+
+    @Test
+    public void shouldNotLoginForInValidLibraryNumber() {
+        final UserInfo userInfo1 = new UserInfo("abc", "abc", "abc");
+        HashMap<String, UserInfo> validUsers = new HashMap<String, UserInfo>() {{
+            put("1234-122", userInfo1);
+        }};
+        UserManager userManager = new UserManager(validUsers);
+
+        assertFalse(userManager.authenticate("1234-123", "abc"));
     }
 }
